bfed25340eafbc6efd450912098e698f5daeaa61
Kaique <kaique.motta@hotmail.com>
2023-10-06 20:30:39 -0300

    feat: add @Autowired

--- fintechADAPessoa/src/main/java/tech/ada/fintechADAPessoa/service/impl/PessoaServiceImpl.java
+++ fintechADAPessoa/src/main/java/tech/ada/fintechADAPessoa/service/impl/PessoaServiceImpl.java
@@ -1,5 +1,6 @@
 package tech.ada.fintechADAPessoa.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.validation.Valid;
@@ -11,6 +12,7 @@ import tech.ada.fintechADAPessoa.service.PessoaService;
 @Service
 public class PessoaServiceImpl implements PessoaService {
 	
+	@Autowired
 	private PessoaRepository repository;
 
 	@Override
