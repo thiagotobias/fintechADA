afde5a1fcae3ef35ea5c97220ddc61e67b93ed47
Kaique <kaique.motta@hotmail.com>
2023-10-06 20:28:57 -0300

    feat: adicionado JsonSubTypes para quando Tipo for Juridoco ir para DTO especifica

--- fintechADAPessoa/src/main/java/tech/ada/fintechADAPessoa/dto/PessoaDTO.java
+++ fintechADAPessoa/src/main/java/tech/ada/fintechADAPessoa/dto/PessoaDTO.java
@@ -1,22 +1,30 @@
 package tech.ada.fintechADAPessoa.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import lombok.Getter;
 import lombok.Setter;
 import tech.ada.fintechADAPessoa.enums.TipoPessoa;
 
 @Getter
 @Setter
+@JsonTypeInfo(property = "tipoPessoa", use = JsonTypeInfo.Id.NAME)
+@JsonSubTypes({ 
+		@JsonSubTypes.Type(value = PessoaFisicaDTO.class, name = "FISICA"),
+		@JsonSubTypes.Type(value = PessoaJuridicaDTO.class, name = "JURIDICA")
+		})
 public abstract class PessoaDTO {
 	private Long id;
-    private String nome;
-    private String email;
-    private String site;
-    private String rua;
-    private String numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String cep;
-    private String estado;
-    private TipoPessoa tipoPessoa;
+	private String nome;
+	private String email;
+	private String site;
+	private String rua;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private String estado;
+	private TipoPessoa tipoPessoa;
 }
